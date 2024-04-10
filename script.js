97b10f657ca00c0f5c183de38dd7591b8fbd4432
martinapbank <martina.peric28@gmail.com>
2024-04-10 19:51:26 +0200

    promjena

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-document.getElementById("contactForm").addEventListener("submit", function(event){
-    event.preventDefault(); // Sprječava podnošenje obrasca
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    // Validacija forme
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
 
-    if(name.trim() === "" || email.trim() === "" || message.trim() === "") {
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
         alert("Molimo ispunite sva polja!");
         return;
     }
 
-    // Ovdje bi se obično slao obrazac putem AJAX-a ili nekog drugog postupka
-    // Za ovaj primjer jednostavno ćemo prikazati poruku o uspješnom slanju
     alert("Poruka je uspješno poslana!\nIme: " + name + "\nEmail: " + email + "\nPoruka: " + message);
 });
